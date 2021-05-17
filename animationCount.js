08500c21e42016737986611c7caf9c92e08a6a4a
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2021-05-17 14:48:33 -0700

    Moving to desktop

---
+++ animationCount.js
@@ -0,0 +1,6 @@
+export default function animationCount() {
+  const shownAnimationArray = localStorage.getItem('animations');
+  //   const text = `You've discovered ${shownAnimationArray} animations!`;
+
+  console.log(localStorage.getItem('animations'));
+}
--- examples.js
+++ examples.js
@@ -1,3 +1,4 @@
+// import animationCount from './animationCount.js';
 import animation1 from './animation1.js';
 import animation2 from './animation2.js';
 import animation3 from './animation3.js';
@@ -6,4 +7,7 @@ import animation5 from './animation5.js';
 
 const animations = [animation1, animation2, animation3, animation4, animation5];
 
-window.onload = animations[Math.floor(Math.random() * animations.length)];
+window.onload = () => {
+  animations[Math.floor(Math.random() * animations.length)];
+  //   animationCount();
+};
